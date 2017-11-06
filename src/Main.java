798b0f0042f23e54c45d9f3be8428327e811c030
silfo795 <a143050@gmail.com>
2017-11-06 15:29:11 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int counter=1;
+        while(counter<=100){
+            System.out.print("*");
+            if(counter%10==0)
+                System.out.print('\n');
+            counter=counter+1;
+
+
+
+
+        }
+    }
+}
